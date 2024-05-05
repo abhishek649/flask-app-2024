5afac1007f3de2deec5829ab3e1575e2ce6b8a9d
abhishek649 <abhishekg528@gmail.com>
2024-05-05 20:51:08 +0530

    push to main branch

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ def hello_world():
 @app.route('/contactus')
 def contact():
     return 'Here is my contact!!'
+@app.route('/aboutus')
+def aboutus():
+    return 'We are an analytics firm working since 2024'
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
