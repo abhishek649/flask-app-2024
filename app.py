ab244df9bd30ba52671f1e34f19566229a126346
abhishek649 <abhishekg528@gmail.com>
2024-05-08 11:27:08 +0530

    push to main branch

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask, render_template # type: ignore
+from google.cloud import pubsub_v1
 
 app = Flask(__name__)
 
@@ -13,6 +14,17 @@ def contact():
 @app.route('/aboutus')
 def aboutus():
     return render_template("aboutus.html")
+@app.route('/callpubsub')
+def pubsub():
+    publisher = pubsub_v1.PublisherClient()
+    topic_name = 'projects/{project_id}/topics/{topic}'.format(
+    project_id='challenge-296807',
+    topic='pubsub-to-bq',  # Set this to something appropriate.
+    )
+    publisher.create_topic(name=topic_name)
+    future = publisher.publish(topic_name, b'My first message!', spam='eggs')
+    future.result()
+
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
