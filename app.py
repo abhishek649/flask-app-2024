25e2c31a4b0412ac5ab1c538809ae072c520897f
abhishek649 <abhishekg528@gmail.com>
2024-05-26 08:36:42 +0530

    push to main

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import os
 
-from flask import Flask, render_template # type: ignore
+from flask import Flask, render_template,request # type: ignore
 from google.cloud import storage
 from utils.sendpubsub import send_message
+import logging
 
 app = Flask(__name__)
-
+cwd = os.getcwd()
+bucket_name='myawesomeapp_upload'
+destination_blob_name='myawsomeapp.pdf'
 @app.route('/')
 def hello_world():
     return render_template("index.html")
@@ -27,6 +30,25 @@ def pubsub():
 @app.route('/processfile')
 def processfile():
     return render_template("fileupload.html")
+@app.route('/upload_document_files', methods=['GET', 'POST'])
+def upload_document_files():
+    try:
+        pdf_file=''
+        if request.method == 'POST':
+            pdf_file=request.files['pdf-file']
+            if pdf_file.filename == '':
+                return render_template("fileupload.html")
+            else:
+                storage_client = storage.Client()
+                bucket = storage_client.bucket(bucket_name)
+                blob = bucket.blob(destination_blob_name)
+                generation_match_precondition = 0
+                blob.upload_from_filename(pdf_file, if_generation_match=generation_match_precondition)
+
+            logging.info(f"File {pdf_file} uploaded to {destination_blob_name}.")
+
+    except Exception as e:
+        logging.info("error to upload file",e)
 
 
 
